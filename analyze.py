0f5c8b8e47be672ac352aeaa57d31800d49f6d57
campionfellin <campionfellin@users.noreply.github.com>
2016-06-11 15:01:46 +0200

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -19,13 +19,20 @@ with open('dataclasses.csv', 'rb') as f:
 	reader = csv.reader(f)
 	totAvg = 0
 	numAvg = 0
+	highestNum = 0
+	highestInfo = ""
 	for row in reader:
 		if row[2].lower().find(className) != -1 and row[4].lower().find(profe) != -1:
 			print row[2]  + " taught by " + row[4][:row[4].find(",")] + " had an average grade of " + row[19] + " for the quarter of " + row[1][7:len(row[1])-1] + " out of " + row[5] + " students"
 			print
 			print
+			if row[19] > highestNum:
+				print "hihi"
+				highestNum = row[19]
+				highestInfo = row[2] + " in " + row[1][7:len(row[1])-1]
 			totAvg = totAvg + float(row[19])
 			numAvg = numAvg + 1
 
 	s = "%3.2f" % (totAvg/numAvg)
+	print "The class with the highest average of those you searched is " + highestInfo + " with an average grade of " + highestNum
 	print "The average grade for this data is " + s
